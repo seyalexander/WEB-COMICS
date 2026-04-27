fb1e43163d0eaef3e33be398e48767679e69b8c0
Edwin <alexande1809piscis@gmail.com>
2026-04-27 00:27:01 -0500

    mejoras menu movil

--- src/app/components/nav-components/nav-components.ts
+++ src/app/components/nav-components/nav-components.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Drawer, DrawerModule } from 'primeng/drawer';
 
 @Component({
   selector: 'app-nav-components',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, Drawer, DrawerModule],
   templateUrl: './nav-components.html',
   styleUrl: './nav-components.css',
 })
 export class NavComponents {
-   menuOpen = false;
+
+  visible4: boolean = false;
+  menuOpen = false;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
